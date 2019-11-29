32e34fd17c6a8f221834d0b38127f33c1e557a8b
oceanway1 <53986381+oceanway1@users.noreply.github.com>
2019-11-28 18:34:18 -0800

    Add files via upload

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const findKey = function(object, callback) {
+
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+console.log(findKey.key);
+
+
+
+
+
+
+
+
+const assertEqual = function (actual, expected) {
+  
+  if (actual === expected) {
+    console.log(` ✅✅✅Assertion Passed: ${actual} ===  ${expected}`);
+  } else if (actual !== expected) {
+    console.log(` 🛑🛑🛑  Assertion Failed:  ${actual} !== ${expected}`);
+  }
+  // TEST CODE  
+}
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
